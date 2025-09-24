97de04dceff31b4ab212002f2e3708513195b0a4
angie <pereiraanngy@gmail.com>
2025-09-24 14:54:00 +0200

    my first commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { sequelize } from './db.ts';
+
+const app = express()
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Ruta de prueba
+app.get('/', (_req, res) => {
+  res.json({ message: 'Asterium API 🚀 funcionando!' });
+});
+
+const PORT = process.env.PORT || 4000;
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('✅ Conectado a MySQL');
+    app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
+  } catch (err) {
+    console.error('❌ Error al conectar a MySQL:', err);
+  }
+})();
