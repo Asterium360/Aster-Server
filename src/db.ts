1a8e3f30a99f3259a7f0900c2c10192aa6d9ff43
angie <pereiraanngy@gmail.com>
2025-09-25 12:27:04 +0200

    testing

--- src/db.ts
+++ src/db.ts
@@ -1,14 +1,18 @@
 import { Sequelize } from 'sequelize';
-import 'dotenv/config';
+
+const dbName: string =
+  process.env.NODE_ENV === 'test'
+    ? (process.env.DB_NAME_TEST ?? process.env.DB_NAME)!
+    : (process.env.DB_NAME!);
 
 export const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
-  process.env.DB_USER as string,
-  process.env.DB_PASSWORD as string,
+  dbName,
+  process.env.DB_USER!,
+  process.env.DB_PASSWORD!,
   {
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT) || 3306,
     dialect: 'mysql',
-    logging: console.log,
+    logging: process.env.NODE_ENV === 'development' ? console.log : false,
   }
 );
--- src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-//import auth from './auth.routes.js';
-//import posts from './posts.routes.js';
-const r = Router();
-//r.use('/auth', auth);
-//r.use('/posts', posts);
-export default r;
