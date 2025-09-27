7e3ff248c20845c4f702787e689c0379815bcc51
MaryoriCruz <maryori.eguizabal@gmail.com>
2025-09-27 03:57:45 -0500

    chore: minor style correction (comma)

--- src/schemas/asterium.schema.ts
+++ src/schemas/asterium.schema.ts
@@ -6,5 +6,6 @@ export const createPostSchema = z.object({
   title: z.string().min(3, "El título debe tener al menos 3 caracteres"), 
   slug: z.string().min(3, "El slug debe tener al menos 3 caracteres"), 
   content_md: z.string().min(10, "El contenido debe tener mínimo 10 caracteres"),
-  status: z.enum(['draft','published','archived']).optional()
-})});
+  status: z.enum(['draft','published','archived']).optional(),
+}),
+});
