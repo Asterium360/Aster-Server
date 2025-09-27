8e90a6ee84f6e7ca04468a43ed4a9a4a461278ac
MaryoriCruz <maryori.eguizabal@gmail.com>
2025-09-27 05:54:58 -0500

    feat(middlewares): add generic validate middleware with Zod support

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -1,1 +1,25 @@
-//aqui
+import type { Request, Response, NextFunction } from "express";
+import type { AnyZodObject } from "zod/v3";
+
+
+//middleware de validacion con zod
+export function validate(schema: AnyZodObject) {
+return (req: Request, res: Response, next: NextFunction) => {
+    try {
+        schema.parse({
+            body: req.body,
+            params: req.params,
+            query: req.query,
+        });
+        next();
+    } catch (err:any){
+        return res.status(400).json({
+            error: "Error de validación",
+            details: err.errors.map((e:any) => ({
+                field: e.path.join("."),
+                message: e.message,
+            })),
+        });
+    } 
+};
+}
