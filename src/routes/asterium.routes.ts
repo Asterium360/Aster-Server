57c760b0a9266006b0cc87f0c1427b56e22083d1
MaryoriCruz <maryori.eguizabal@gmail.com>
2025-09-29 02:42:09 -0500

    feat(routes): add mock routes for auth (register, login) and discoveries (CRUD)

--- src/routes/asterium.routes.ts
+++ src/routes/asterium.routes.ts
@@ -1,1 +1,50 @@
-//aqui
+import { Router } from "express";
+import { requireAuth } from "../middlewares/auth.js";
+import { validate } from "../middlewares/validate.js";
+import { createPostSchema } from "../schemas/asterium.schema.js"; 
+import type { id } from "zod/locales";
+
+const router = Router();
+
+//Crear un descubriemiento (Este requiere login)
+router.post("/", requireAuth, validate(createPostSchema), (req, res)=>{
+    res.json({
+        message: "Post creado correctamente ✅",
+        data: req.body,
+    });
+});
+
+//Listar todos los descubriemientos
+router.get("/", (_req, res)=>{
+    res.json({
+        message: "Lista de descubrimientos ✅",
+        data: [],
+    });
+});
+
+//Obtener un descubrimiento por ID
+router.get("/:id", (req,res)=> {
+    res.json({
+        message: "Detalle del descubrimiento ✅",
+        id: req.params.id,
+    });
+});
+
+//Actualizar el descubrimiento por ID
+router.put("/:id", requireAuth, validate(createPostSchema), (req, res)=> {
+    res.json({
+        message: "Descubrimiento actualizado ✅",
+        id: req.params.id,
+        data: req.body,
+    });
+});
+
+//Eliminar un descubrimiento por ID, esto requiere login
+router.delete("/:id", requireAuth, (req, res) => {
+    res.json({
+        message: "Descubrimiento eliminado ✅",
+        id: req.params.id
+    });
+});
+
+export default router;
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,1 +1,24 @@
-//aqui
+import { Router } from "express";
+import { validate } from "../middlewares/validate.js";
+import { registerSchema, loginSchema } from "../schemas/auth.schema.js";
+
+const router = Router(); 
+
+//Registro de usuario
+router.post("/resgister", validate(registerSchema), (req, res) =>{
+    res.json ({
+        message: "Registro válido ✅",
+        data: req.body,
+    });
+});
+
+//Login de usuario 
+router.post("/login", validate(loginSchema), (req, res)=> {
+    res.json({
+        message: "Login válido ✅",
+        data: req.body,
+    });
+});
+
+export default router;
+
