53b87e9ad6c086c2624da42e47d5999421da2e7d
Sofiareyes12 <121201sofiareyes@gmail.com>
2025-10-09 15:40:39 +0200

    Feat new seeders for db

--- src/models/Asterium.ts
+++ src/models/Asterium.ts
@@ -1,36 +1,38 @@
 import { DataTypes, Model } from 'sequelize';
 import type { Optional } from 'sequelize';
 import { sequelize } from '../db.js';
+import { url } from 'inspector';
 
 export interface AsteriumAttrs {
   id: number;
   author_id: number;                  // Foreign key to User model
   title: string;
-  slug: string;
   excerpt?: string | null;
   content_md: string;
   status: 'draft' | 'published' | 'archived';
   published_at?: Date | null;
   like_count: number;
+  image_url?: string|null;
   created_at?: Date;
   updated_at?: Date;
+
 }
 
-type AsteriumCreation = Optional<
+export type AsteriumCreation = Optional<
   AsteriumAttrs,
-  'id' | 'excerpt' | 'published_at' | 'like_count' | 'created_at' | 'updated_at'
+  'id' | 'excerpt' | 'published_at' | 'like_count' | 'image_url' | 'created_at' | 'updated_at'
 >;
 
 export class Asterium extends Model<AsteriumAttrs, AsteriumCreation> implements AsteriumAttrs {
   declare id: number;
   declare author_id: number;
   declare title: string;
-  declare slug: string;
   declare excerpt: string | null;
   declare content_md: string;
   declare status: 'draft' | 'published' | 'archived';
   declare published_at: Date | null;
   declare like_count: number;
+  declare image_url: string | null;
   declare readonly created_at: Date;
   declare readonly updated_at: Date;
 }
@@ -40,12 +42,12 @@ Asterium.init(
     id: { type: DataTypes.INTEGER.UNSIGNED, autoIncrement: true, primaryKey: true },
     author_id: { type: DataTypes.INTEGER.UNSIGNED, allowNull: false },
     title: { type: DataTypes.STRING(200), allowNull: false },
-    slug: { type: DataTypes.STRING(220), allowNull: false, unique: true },
     excerpt: { type: DataTypes.TEXT, allowNull: true, defaultValue: null },
     content_md: { type: DataTypes.TEXT('long'), allowNull: false },
     status: { type: DataTypes.ENUM('draft', 'published', 'archived'), allowNull: false, defaultValue: 'draft' },
     published_at: { type: DataTypes.DATE, allowNull: true, defaultValue: null },
     like_count: { type: DataTypes.INTEGER.UNSIGNED, allowNull: false, defaultValue: 0 },
+    image_url: { type: DataTypes.STRING, allowNull: true, defaultValue: null, validate: { isUrl: {msg: 'La imagen debe ser una url valida (http o https)'} } },
     created_at: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
     updated_at: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
   },
